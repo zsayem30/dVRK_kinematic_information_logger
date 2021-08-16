ba8b33630a2eb584044034e1598d2fc15437c0e6
zsayem30 <sayemnazmuz@gmail.com>
2021-08-15 20:23:50 -0700

    Fixed some minor bugs, replay motion now works

--- replay_edited.py
+++ replay_edited.py
@@ -10,15 +10,17 @@ import PyKDL
 import pandas as pd
 from numpy import linalg as LA
 
-data = pd.read_excel(r'dvrk_kinematic_logger_test.xls')
+data = pd.read_excel(r'dvrk_kinematic_logger_final_test.xls')
 df = pd.DataFrame(data)
 
 data_array = df.to_numpy()
-df_header = list(df.columns)
+df_header = list(df.columns)[3:10] + list(df.columns)[42:49] + list(df.columns)[81:85]
 
-
+print(df_header)
 
 time = data_array[:,1]
+
+print(time)
 
 psm1_joint_values = data_array[:, 3:9]
 psm1_jaw = data_array[:, 9]
@@ -27,15 +29,10 @@ psm3_jaw = data_array[:, 48]
 
 ecm_joint_values = data_array[:, 81:85]
 
-print(psm1_jaw[0])
-print(psm3_jaw[0])
-
 delay = [t - s for s, t in zip(time, time[1:])]
 
-
-
-psm1 = dvrk.arm('PSM1')
-psm3 = dvrk.arm('PSM3')
+psm1 = dvrk.psm('PSM1')
+psm3 = dvrk.psm('PSM3')
 ecm = dvrk.arm('ECM')
 
 psm1.home()
@@ -45,25 +42,25 @@ ecm.home()
 psm1_replayed_joint_values = np.empty([len(psm1_joint_values), len(psm1_joint_values[0,:])])
 psm1_replayed_jaw_values = np.empty([len(psm1_jaw), 1])
 psm3_replayed_joint_values = np.empty([len(psm3_joint_values), len(psm3_joint_values[0,:])])
-psm1_replayed_jaw_values = np.empty([len(psm3_jaw), 1])
+psm3_replayed_jaw_values = np.empty([len(psm3_jaw), 1])
 ecm_replayed_joint_values = np.empty([len(ecm_joint_values), len(ecm_joint_values[0,:])])
 
 goal_start_psm1 = np.copy(psm1_joint_values[0])
 goal_start_psm3 = np.copy(psm3_joint_values[0])
 goal_start_ecm = np.copy(ecm_joint_values[0])
 
-goal_psm1_jaw = np.copy(psm1_jaw_values[0])
-goal_psm3_jaw = np.copy(psm3_jaw_values[0])
+goal_psm1_jaw = np.copy(psm1_jaw[0])
+goal_psm3_jaw = np.copy(psm3_jaw[0])
 
 print('Moving arms to initial starting position...')
 
-psm1.move_joint(goal_psm1, interpolate = False)
+psm1.move_joint(goal_start_psm1, interpolate = False)
 psm1.move_jaw(goal_psm1_jaw, interpolate = False)
 
-psm3.move_joint(goal_psm3, interpolate = False)
+psm3.move_joint(goal_start_psm3, interpolate = False)
 psm3.move_jaw(goal_psm3_jaw, interpolate = False)
 
-ecm.move_joint(goal_ecm, interpolate = False)
+ecm.move_joint(goal_start_ecm, interpolate = False)
 
 rospy.sleep(2)
 
@@ -82,8 +79,8 @@ for i in range(len(delay)):
 	goal_psm3 = np.copy(psm3_joint_values[i + 1])
 	goal_ecm = np.copy(ecm_joint_values[i + 1])
 
-	goal_psm1_jaw = np.copy(PSM1_jaw[i + 1])
-	goal_psm3_jaw = np.copy(PSM3_jaw[i + 1])
+	goal_psm1_jaw = np.copy(psm1_jaw[i + 1])
+	goal_psm3_jaw = np.copy(psm3_jaw[i + 1])
 
 	psm1.move_joint(goal_psm1, interpolate = False)
 	psm1.move_jaw(goal_psm1_jaw, interpolate = False)
@@ -103,16 +100,16 @@ for i in range(len(delay)):
 	
 	ecm_replayed_joint_values[i + 1, :] = ecm.get_current_joint_position()
 
-psm1_replayed_data = np.concatenate((psm1_replayed_joint_values, psm1_replayed_jaw_values), axis = 0)
-psm3_replayed_data = np.concatenate((psm3_replayed_joint_values, psm3_replayed_jaw_values), axis = 0)
+psm1_replayed_data = np.hstack((psm1_replayed_joint_values, psm1_replayed_jaw_values))
+psm3_replayed_data = np.hstack((psm3_replayed_joint_values, psm3_replayed_jaw_values))
 
-all_data = np.concatenate((psm1_replayed_data, psm3_replayed_data, ecm_replayed_joint_values), axis = 0)
+all_data = np.hstack((psm1_replayed_data, psm3_replayed_data, ecm_replayed_joint_values))
 
 replayed_joint_values_Df = pd.DataFrame(all_data, columns = df_header)
 
-filepath = 'replayed_data.xlsx'
+filepath = 'replayed_data_final.xlsx'
 
-psm1_replayed_joint_values_Df.to_excel(filepath, index = False)
+replayed_joint_values_Df.to_excel(filepath, index = False)
 
 print(rospy.get_caller_id(), ' <- joint direct complete')
 
